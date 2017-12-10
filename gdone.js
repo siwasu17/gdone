ea89df89089f9f7f98339a0b8f061748327b5003
siwasu17 <siwasu17@gmail.com>
2017-12-10 22:27:52 +0900

    Add argv

--- gdone.js
+++ gdone.js
@@ -1,8 +1,19 @@
 #!/usr/bin/env node
+const argv = require('argv');
 const googlehome = require('google-home-notifier')
 const language = 'en';
+ 
+argv.option({
+	name: 'command',
+	short: 'c',
+	type : 'string',
+	description :'Additional text to notify',
+	example: "'script --command=value' or 'script -c value'"
+});
 
+const args = argv.run();
+let commandName = args.options.command ? args.options.command : '';
 googlehome.device('Google-Home', language); 
-googlehome.notify('Command complete!', function(res) {
+googlehome.notify(commandName +  ' command complete!', function(res) {
 	console.log(res);
 });
